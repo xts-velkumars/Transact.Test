7ea8f14019eaa591ed7855cc8d5400c7f4ffde73
xts-velkumars <velkumars26@gmail.com>
2020-02-17 23:56:27 +0530

    Add files via upload

---
+++ Transact.Test/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Reflection;
+
+namespace Transact.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter lower value ");
+            var lowerValue = GetValue();
+            Console.WriteLine("Your input: {0}", lowerValue);
+
+            Console.WriteLine("Please enter higher value ");
+            var higherValue = GetValue();
+            Console.WriteLine("Your input: {0}", higherValue);
+
+
+            Console.WriteLine("Please enter first value ");
+            var a = GetValidValue(lowerValue, higherValue);
+            Console.WriteLine("Your input: {0}", a);
+
+            Console.WriteLine("Please enter second value ");
+            var b = GetValidValue(lowerValue, higherValue);
+            Console.WriteLine("Your input: {0}", b);
+
+            for (int i = lowerValue; i < higherValue; i++)
+            {
+                IsDivisible(i, a, b);
+            }
+
+            Console.ReadKey();
+
+        }
+
+        private static int GetValue()
+        {
+            int choice;
+            while (!int.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine("Wrong input! Please enter numeric value");
+            }
+
+             
+            return choice;
+        }
+
+        private static int GetValidValue(int lowerValue, int higherValue)
+        {
+            var result = GetValue();
+
+            if (result >= lowerValue && result <= higherValue) 
+                return result;
+
+            Console.WriteLine($"Invalid Input, Please enter a value between {lowerValue} to {higherValue} ");
+            GetValidValue(lowerValue, higherValue);
+
+            return result;
+        }
+
+        private static void IsDivisible(int currentIndex, int a, int b)
+        {
+            if (currentIndex % a == 0)
+            {
+                Console.WriteLine("Fancy");
+            }
+
+            if (currentIndex % b == 0)
+            {
+                Console.WriteLine("Pants");
+            }
+
+            if (currentIndex % a == 0 && currentIndex % b == 0)
+            {
+                Console.WriteLine("FancyPants");
+            }
+        }
+    }
+}
---
+++ Transact.Test/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace Transact.Test
+{
+    public static class StringExtensions
+    {
+        public static bool IsNumeric(this string value, bool floatPoint = false)
+        {
+            return long.TryParse(value.RemoveSpaces(), NumberStyles.Integer, NumberFormatInfo.InvariantInfo, out _);
+        }
+
+        public static string RemoveSpaces(this string value)
+        {
+            return IsNullOrEmpty(value) ? string.Empty : value.Replace(" ", string.Empty);
+        }
+
+        public static bool IsNullOrEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+    }
+}
